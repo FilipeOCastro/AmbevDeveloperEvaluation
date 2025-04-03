10523ee74549c559f71e6e83d39fb0d28c363995
FilipeOCastro <filipe.oliveira.castro@gmail.com>
2025-04-03 19:47:16 -0300

    fix docker

---
+++ template/backend/scripts/script-database.sql
@@ -0,0 +1,77 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" character varying(150) NOT NULL,
+    "ProductVersion" character varying(32) NOT NULL,
+    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+
+START TRANSACTION;
+
+CREATE TABLE "Users" (
+    "Id" uuid NOT NULL DEFAULT (gen_random_uuid()),
+    "Username" character varying(50) NOT NULL,
+    "Password" character varying(100) NOT NULL,
+    "Phone" character varying(20) NOT NULL,
+    "Email" character varying(100) NOT NULL,
+    "Status" character varying(20) NOT NULL,
+    "Role" character varying(20) NOT NULL,
+    CONSTRAINT "PK_Users" PRIMARY KEY ("Id")
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20241014011203_InitialMigrations', '8.0.10');
+
+COMMIT;
+
+START TRANSACTION;
+
+ALTER TABLE "Users" ADD "CreatedAt" timestamp with time zone NOT NULL DEFAULT TIMESTAMPTZ '-infinity';
+
+ALTER TABLE "Users" ADD "UpdatedAt" timestamp with time zone;
+
+CREATE TABLE "Cart" (
+    "Id" uuid NOT NULL,
+    "Number" character varying(50) NOT NULL,
+    "CreateDate" timestamp with time zone NOT NULL,
+    "UserId" uuid NOT NULL,
+    "TotalAmount" numeric(18,2) NOT NULL,
+    "BranchName" character varying(100) NOT NULL,
+    "IsCancelled" boolean NOT NULL,
+    CONSTRAINT "PK_Cart" PRIMARY KEY ("Id"),
+    CONSTRAINT "FK_Cart_Users_UserId" FOREIGN KEY ("UserId") REFERENCES "Users" ("Id") ON DELETE RESTRICT
+);
+
+CREATE TABLE "Product" (
+    "Id" uuid NOT NULL,
+    "Title" character varying(100) NOT NULL,
+    "Price" numeric(18,2) NOT NULL,
+    "Description" character varying(500) NOT NULL,
+    "Category" character varying(100) NOT NULL,
+    "Image" character varying(500) NOT NULL,
+    "Rate" numeric NOT NULL,
+    "Count" integer NOT NULL,
+    CONSTRAINT "PK_Product" PRIMARY KEY ("Id")
+);
+
+CREATE TABLE "CartItem" (
+    "Id" uuid NOT NULL,
+    "ProductId" uuid NOT NULL,
+    "Quantity" integer NOT NULL,
+    "UnitPrice" numeric(18,2) NOT NULL,
+    "Discount" numeric(18,2) NOT NULL,
+    "TotalPrice" numeric(18,2) NOT NULL,
+    "CartId" uuid NOT NULL,
+    CONSTRAINT "PK_CartItem" PRIMARY KEY ("Id"),
+    CONSTRAINT "FK_CartItem_Cart_CartId" FOREIGN KEY ("CartId") REFERENCES "Cart" ("Id") ON DELETE CASCADE,
+    CONSTRAINT "FK_CartItem_Product_ProductId" FOREIGN KEY ("ProductId") REFERENCES "Product" ("Id") ON DELETE RESTRICT
+);
+
+CREATE INDEX "IX_Cart_UserId" ON "Cart" ("UserId");
+
+CREATE INDEX "IX_CartItem_CartId" ON "CartItem" ("CartId");
+
+CREATE INDEX "IX_CartItem_ProductId" ON "CartItem" ("ProductId");
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250403182537_AddTables', '8.0.10');
+
+COMMIT;
--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Program.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Program.cs
@@ -8,7 +8,6 @@ using Ambev.DeveloperEvaluation.ORM;
 using Ambev.DeveloperEvaluation.WebApi.Middleware;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Serilog;
 
 namespace Ambev.DeveloperEvaluation.WebApi;
 
@@ -16,79 +15,58 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        try
-        {
-            Log.Information("Starting web application");
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        builder.AddDefaultLogging();
 
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-            builder.AddDefaultLogging();
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
 
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
+        builder.AddBasicHealthChecks();
+        builder.Services.AddSwaggerGen();
 
-            builder.AddBasicHealthChecks();
-            builder.Services.AddSwaggerGen();
-
-            //builder.Services.AddDbContext<DefaultContext>(options =>
-            //{
-            //    options.UseInMemoryDatabase("InMemoryDb");// Provedor em memória
-
-            //    //options.UseNpgsql(
-            //    //    builder.Configuration.GetConnectionString("DefaultConnection"),
-            //    //    b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.ORM")
-            //    //);
-            //});
-
-            builder.Services.AddDbContext<DefaultContext>(options =>
-                options.UseNpgsql(
+        builder.Services.AddDbContext<DefaultContext>(options =>
+            options.UseNpgsql(
                 builder.Configuration.GetConnectionString("DefaultConnection"),
-                 b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.ORM")
-                )
+                b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.ORM")
+            )
+        );
+
+        builder.Services.AddJwtAuthentication(builder.Configuration);
+
+        builder.RegisterDependencies();
+
+        builder.Services.AddAutoMapper(typeof(Program).Assembly, typeof(ApplicationLayer).Assembly);
+
+        builder.Services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssemblies(
+                typeof(ApplicationLayer).Assembly,
+                typeof(Program).Assembly
             );
+        });
 
-            builder.Services.AddJwtAuthentication(builder.Configuration);
+        builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
-            builder.RegisterDependencies();
+        var app = builder.Build();
+        app.UseMiddleware<ValidationExceptionMiddleware>();
 
-            builder.Services.AddAutoMapper(typeof(Program).Assembly, typeof(ApplicationLayer).Assembly);
-
-            builder.Services.AddMediatR(cfg =>
-            {
-                cfg.RegisterServicesFromAssemblies(
-                    typeof(ApplicationLayer).Assembly,
-                    typeof(Program).Assembly
-                );
-            });
-
-            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-
-            var app = builder.Build();
-            app.UseMiddleware<ValidationExceptionMiddleware>();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        else        
             app.UseHttpsRedirection();
+        
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+        app.UseAuthentication();
+        app.UseAuthorization();
 
-            app.UseBasicHealthChecks();
+        app.UseBasicHealthChecks();
 
-            app.MapControllers();
+        app.MapControllers();
 
-            app.Run();
-        }
-        catch (Exception ex)
-        {
-            Log.Fatal(ex, "Application terminated unexpectedly");
-        }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
+        app.Run();
+
     }
 }
